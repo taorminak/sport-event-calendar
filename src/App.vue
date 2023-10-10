5ca1ed0985ad1b7d3cc298f6f26e6b5ef81b480f
taorminak <ostanina.anna@yahoo.com>
2023-10-10 13:27:18 +0200

    feat: toggle theme

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,55 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div class="app light">
+  <div class="app" :class="themeClass">
     <header>
       <nav>
+        <button class="header__button-mode" @click="toggleTheme">{{ themeLabel }}</button>
         <div>Hello</div>
       </nav>
     </header>
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  data() {
+    return {
+      isLightTheme: true,
+    };
+  },
+  computed: {
+    themeClass(): string {
+      return this.isLightTheme ? 'light' : 'dark';
+    },
+    themeLabel(): string {
+      return this.isLightTheme ? 'Night Mode' : 'Light Mode';
+    },
+  },
+  methods: {
+    toggleTheme(): void {
+      this.isLightTheme = !this.isLightTheme;
+    },
+  },
+});
+</script>
+
 <style lang="scss">
 @import './assets/styles/index.scss';
+
+.header__button-mode {
+  padding: 10px 20px;
+  background-color: #ac9dc5;
+  color: #3b3a3d;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  transition:
+    background-color 0.3s ease,
+    color 0.3s ease;
+
+  &:hover {
+    background-color: #8b7aa8;
+  }
+}
 </style>
